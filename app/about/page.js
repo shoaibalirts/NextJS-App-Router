d4e047ca6064d8be3cd0d31d032ee29782cac536
Shoaibalirts <shoaibali.rts@gmail.com>
2024-10-21 13:59:33 +0200

    add Header component

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+    console.log('Hello');
+  return (
+    <>
+      <p>About me</p>
+      <Link href="/">Home Page</Link>
+    </>
+  );
+}
